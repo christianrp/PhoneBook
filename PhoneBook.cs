b4b2e3bdd85d9fcd7f1de4803332272fdd532cc2
christianrp <christianr@ymail.com>
2017-04-08 17:59:35 -0700

    add code to main method

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -7,7 +7,35 @@ class PhoneBook
 
     static void Main() 
     {
-
+        Console.WriteLine("MAIN MENU");
+        Console.WriteLine("Would you like to add a person to your phone book? ['Y' for yes, 'Enter' for no]");
+        string answer = Console.ReadLine();
+        if (answer == "Y" || answer == "y")
+        {
+            AddContact();
+        }
+        else
+        {
+            Console.WriteLine("Would you like to lookup a number in your phone book? ['Y' for yes, 'Enter' for no]");
+            string.lookUpAnswer = Console.ReadLine();
+            if (lookUpAnswer == "Y" || lookUpAnswer == "y")
+            {
+                LookUpContact();
+            }
+            else
+            {
+                Console.WriteLine("Are you finished with this program? ['Y' for yes, 'Enter' for no]");
+                string finishedAnswer = Console.ReadLine();
+                if (finishedAnswer == "Y" || finishedAnswer == "y")
+                {
+                    Console.WriteLine("Goodbye.");
+                }
+                else
+                {
+                    Main();
+                }
+            }
+        }
     }
 
     static void AddContact() {
@@ -15,6 +43,6 @@ class PhoneBook
     }
 
     static void LookUpContact() {
-        
+
     }
 }
